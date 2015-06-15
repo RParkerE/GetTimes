dc79c66b882188e53a5f428165025a39091bf363
Robert Parker Ellwanger <pellwanger@aol.com>
2015-06-15 08:34:26 -0400

    Update riseandset.ino

--- examples/riseandset.ino
+++ examples/riseandset.ino
@@ -4,8 +4,6 @@ void setup {
   GetTimes();
 }
 void loop {
-  sunrise();
-  sunset();
   Serial.println("Sunrise is at: " + convertToTime(sunrise()));
   Serial.println("Sunset is at: " + convertToTime(sunset()));
   delay(79200000);
