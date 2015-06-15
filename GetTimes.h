f855900fefc317d2f507b8ae6059b09bc463aa1c
Robert Parker Ellwanger <pellwanger@aol.com>
2015-06-14 20:36:24 -0400

    Create GetTimes.h

---
+++ GetTimes.h
@@ -0,0 +1,31 @@
+/*
+  GetTimes.h - Library for getting the sunrise and
+  sunset times, as well as if it is daylight savings
+  or not for a specified location, or for your location.
+  Also is able to find your position when using internet.
+  Created by Robert P. Ellwanger, June 14, 2015.
+  Licensed under Apache License v2.0.
+*/
+
+#ifndef GetTimes_h
+#define GetTimes_h
+
+#include "Arduino.h"
+
+class GetTimes
+{
+  public:
+    GetTimes();
+    GetTimes(String lon, String lat);
+    float sunrise();
+    float sunset();
+    boolean daylightSavings();
+    String getLon();
+    String getLat();
+	String convertToTime(float n)
+  private:
+    int _lon;
+    int _lat;
+};
+
+#endif
